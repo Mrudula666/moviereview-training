53b274b31d00330b6a11e54e166910203f4c9515
Nimmala <mrudula.nimmala@capgemini.com>
2020-02-24 13:13:12 +0530

    deleted movie info service

--- movieinfoservice/src/main/java/com/moviereviews/movieinfoservice/repo/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.moviereviews.movieinfoservice.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.moviereviews.movieinfoservice.model.Movie;
-
-public interface MovieRepository extends MongoRepository<Movie, Integer> {
-
-}
